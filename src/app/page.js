e418b4815c48c19160b7db37352099fc5d493198
Raffi14 <Raffifabiansyah@gmail.com>
2024-07-27 15:37:33 +0700

    Create About Section

--- src/app/page.js
+++ src/app/page.js
@@ -108,7 +108,7 @@ function Home()
     <>
       <div id="Home" className="flex flex-col-reverse h-lvh md:flex-row relative w-full bg-[url('../../public/Source_image/background.jpg')] bg-no-repeat bg-cover to-slate-800 ">
         <div className="flex-1">
-          <div className="my-5 md:my-48 xl:my-60">
+          <div className="my-5 md:my-48 xl:mt-[30vh]">
           <h1 className="ml-10 md:ml-32 text-2xl font-sans sm:text-3xl md:text-4xl xl:text-5xl font-bold antialiased text-sky-800">HI,</h1>
           <span className="ml-10 md:ml-32 mt-2 text-2xl font-sans sm:text-3xl md:text-4xl xl:text-5xl font-bold antialiased text-sky-800">I'm <span className="text-yellow-600"> Raffi Fabiansyah</span></span>
           <div className="ml-10 md:ml-32 mt-2 text-xs sm:text-sm md:text-xl xl:text-2xl font-sans font-bold flex gap-2">
@@ -126,24 +126,26 @@ function Home()
               }}/> 
             </span>
           </div>
-          <button className="ml-10 mt-5 w-32 h-10 rounded-md bg-sky-800 font-sans font-bold md:mt-10 md:ml-32">Contact</button>
-          <div className="flex mt-11 md:my-36 xl:my-60 gap-8">
+          <button className="ml-10 mt-5 w-32 h-10 rounded-md bg-sky-800 font-sans font-bold md:mt-10 md:ml-32">Contact Me</button>
+          <div className="flex mt-11 md:my-36 xl:mt-[30vh] gap-8">
             <a href="https://www.linkedin.com/in/raffi-fabiansyah-a6654031b/"><button className="ml-10 w-7 h-7 rounded-md md:ml-36 bg-[url('../../public/Source_image/LinkedIn.png')] bg-no-repeat bg-cover"></button></a>
             <a href="https://github.com/Raffi14"><button className="w-7 h-7 rounded-md  bg-[url('../../public/Source_image/GitHub_Logo.png')] bg-no-repeat bg-cover"></button></a>
           </div>
           </div>
         </div>
         <div className="flex-1 px-10">
-          <motion.div onMouseMove={HandleMouseMove} onMouseLeave={HandleMouseLeave} style={{
-            rotateX,
-            rotateY,
-            transformStyle: "preserve-3d",
-          }} className="relative rounded-full bg-gradient-to-br from-indigo-300 to-violet-300 mx-auto my-24 md:my-40 xl:my-36 xl:h-[500px] xl:w-[500px] md:h-[300px] md:w-[300px] h-[200px] w-[200px]">
-            <div style={{
-              transform: "translateZ(75px)",
+          <div className="mt-[20vh]">
+            <motion.div onMouseMove={HandleMouseMove} onMouseLeave={HandleMouseLeave} style={{
+              rotateX,
+              rotateY,
               transformStyle: "preserve-3d",
-            }} className="absolute inset-5 grid place-content-center rounded-full bg-[url('../../public/Source_image/profile.png')] bg-no-repeat bg-cover bg-center"></div>
-          </motion.div>
+            }} className="relative rounded-full bg-gradient-to-br from-indigo-300 to-violet-300 mx-auto my-24 md:my-40 xl:my-36 xl:h-[500px] xl:w-[500px] md:h-[300px] md:w-[300px] h-[200px] w-[200px]">
+              <div style={{
+                transform: "translateZ(75px)",
+                transformStyle: "preserve-3d",
+              }} className="absolute inset-5 grid place-content-center rounded-full bg-[url('../../public/Source_image/profile.png')] bg-no-repeat bg-cover bg-center"></div>
+            </motion.div>
+          </div>
         </div>
       </div>
     </>
@@ -153,7 +155,15 @@ function Home()
 function About(){
   return(
     <>
-      <div className="w-full h-screen bg-slate-600 " id="About"></div>
+      <div className="flex flex-col md:flex-row w-full h-lvh bg-slate-600" id="About">
+        <div className="flex-1  bg-[url('../../public/Source_image/profile.png')] bg-no-repeat bg-cover grayscale bg-center max-[768px]:border-b-4 md:border-r-4"></div>
+        <div className="flex-1">
+          <h1 className="text-center mt-5 md:mt-24 xl:mt-44 text-2xl font-sans sm:text-3xl md:text-4xl xl:text-5xl font-bold antialiased text-white">About Me</h1>
+          <div className="w-3/4 mx-auto mt-[2vh] md:mt-[10vh]">
+              <p className="font-sans text-sm md:text-xl text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
