a5e7afe3ee49e934c4bc3519b88f95cb9dffa84e
Raffi14 <Raffifabiansyah@gmail.com>
2024-09-10 11:31:59 +0700

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    use: {
-        extraHTTPHeaders: {
-          'x-vercel-protection-bypass': "gx2q2PoKvJdbUtbonyHuEWCLrU6rikNQ",
-          'x-vercel-set-bypass-cookie': true,
-        }
-      },
 };
 
 export default nextConfig;
