df86b3ae3a28b60ee415d8d0cdf58b93760d5570
Raffi Fabiansyah <133511291+Raffi14@users.noreply.github.com>
2025-05-06 12:07:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    output: 'export', // Tambahkan ini untuk ekspor statis
+    distDir: 'out'
 };
 
 export default nextConfig;
